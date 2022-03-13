137164edf3c299ea7cf6acc1cb12474ee1de7c74
Andrew Kim <andrewk745@gmail.com>
2022-03-13 05:04:44 -0400

    made test file for Navbar

--- src/Components/Navbar.test.js
+++ src/Components/Navbar.test.js
@@ -7,5 +7,9 @@ import Navbar from './Navbar'
 test("Navbar renders properly", () => {
     render(<Navbar/>)
 
-    
+    let navbar = screen.getByRole('navigation')
+    let hamburger = screen.getByRole('button', {name: /Toggle Navigation/i})
+    expect(navbar).toBeInTheDocument();
+    expect(hamburger).toBeInTheDocument();
+
 })
