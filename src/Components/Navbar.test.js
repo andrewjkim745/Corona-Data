aa36acf517920b562dd3c308d85d706d2463d73c
Andrew Kim <andrewk745@gmail.com>
2022-03-13 23:27:25 -0400

    added click test

--- src/Components/Navbar.test.js
+++ src/Components/Navbar.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Navbar from './Navbar'
 
 
@@ -11,5 +12,9 @@ test("Navbar renders properly", () => {
     let hamburger = screen.getByRole('button', {name: /Toggle Navigation/i})
     expect(navbar).toBeInTheDocument();
     expect(hamburger).toBeInTheDocument();
+
+    userEvent.click(hamburger)
+
+    expect(hamburger).toHaveBeenCalledTimes(1)
 
 })
