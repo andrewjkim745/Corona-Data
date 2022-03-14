3baad558227c15578f8768f3313dafddce3f8b15
Andrew Kim <andrewk745@gmail.com>
2022-03-14 16:00:54 -0400

    created features test component

---
+++ src/Components/Features/Features.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Features from './Features';
+
+
+
+
+
+test("Features component renders properly", () => {
+    render(<Features/>)
+
+
+    
+})
