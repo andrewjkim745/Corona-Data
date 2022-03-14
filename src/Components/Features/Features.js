0ca9d91c8ec64a717dc380e7ef37af73e2c81109
Andrew Kim <andrewk745@gmail.com>
2022-03-14 18:13:29 -0400

    added more to features component

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -9,9 +9,11 @@ export default function Features() {
     <div class='container'>
         <div class='row'>
             <div class='col-8'>
+                <h1>Concept of component layout</h1>
                 <img src={layout} alt='Layout'/>
             </div>
             <div class='col-4'>
+            <h1>Layout will display map and graph</h1>
             <img src={layout2} alt='Layout2'/>
             <img src={layout3} alt='Layout3'/>
             </div>
