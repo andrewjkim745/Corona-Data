64367f2e54b36e1cb28a8bd5f3d2be6e0cd2217f
Andrew Kim <andrewk745@gmail.com>
2022-03-13 23:31:46 -0400

    removed navbar link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,6 @@ export default function AppNavbar() {
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
