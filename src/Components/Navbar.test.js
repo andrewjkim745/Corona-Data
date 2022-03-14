d9db60115472d9e031e2e20005f9a2e3cbc20eb1
Andrew Kim <andrewk745@gmail.com>
2022-03-14 02:14:58 -0400

    wrote comment as reminder

--- src/Components/Navbar.test.js
+++ src/Components/Navbar.test.js
@@ -7,19 +7,17 @@ import Navbar from './Navbar'
 
 test("Navbar renders properly", () => {
     render(<Navbar/>)
-
     let navbar = screen.getByRole('navigation')
     let hamburger = screen.getByRole('button', {name: /Toggle Navigation/i})
     let dropdown = screen.getByText('Dropdown')
     expect(navbar).toBeInTheDocument();
     expect(hamburger).toBeInTheDocument();
     expect(dropdown).toBeInTheDocument();
-    
     userEvent.click(hamburger)
     expect(hamburger).toHaveClass('navbar-toggler')
     userEvent.click(dropdown)
     let dropdownEle = screen.getByText('Action')
     expect(dropdownEle).toBeInTheDocument();
-    
 
+    // research closest function
 })
