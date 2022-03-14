0d548584d52bc865016f0a15a0306c1ff6dc7fb1
Andrew Kim <andrewk745@gmail.com>
2022-03-14 18:12:10 -0400

    imported images into features component

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import layout from '../../assets/layout.jpg'
+import layout2 from '../../assets/layout2.jpg'
+import layout3 from '../../assets/layout3.jpg'
 
 
 
 export default function Features() {
-    <>
-    Features
-    </>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-8'>
+                <img src={layout} alt='Layout'/>
+            </div>
+            <div class='col-4'>
+            <img src={layout2} alt='Layout2'/>
+            <img src={layout3} alt='Layout3'/>
+            </div>
+        </div>
+    </div>
 }
