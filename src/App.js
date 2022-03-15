2822eb3893e50e570cb527eec40c97297e15dc94
Andrew Kim <andrewk745@gmail.com>
2022-03-15 00:24:36 -0400

    added features into app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './Variables/Variables.scss'
-
 import Navbar from './Components/Navbar/Navbar';
+import Features from './Components/Features/Features';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Features/>
     </div>
   );
 }
