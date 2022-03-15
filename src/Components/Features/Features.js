6ab95d43a8a8879a6db3430a11a5d2166ace6245
Andrew Kim <andrewk745@gmail.com>
2022-03-15 00:09:03 -0400

    changed image classes

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -10,12 +10,12 @@ export default function Features() {
         <div class='row'>
             <div class='col-8'>
                 <h1>Concept of component layout</h1>
-                <img src={layout} alt='Layout'/>
+                <img class='img-fluid' src={layout} alt='Layout'/>
             </div>
             <div class='col-4'>
             <h1>Layout will display map and graph</h1>
-            <img src={layout2} alt='Layout2'/>
-            <img src={layout3} alt='Layout3'/>
+            <img class='img-thumbnail' src={layout2} alt='Layout2'/>
+            <img class='img-thumbnail' src={layout3} alt='Layout3'/>
             </div>
         </div>
     </div>
