74f4916338a67ec47f5d2ba84d8853a356be4b91
Andrew Kim <andrewk745@gmail.com>
2022-03-16 00:37:09 -0400

    changed class to classname

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -7,18 +7,18 @@ import layout3 from '../../assets/layout3.jpg'
 
 export default function Features() {
     return (
-<div class='container my-5'>
-        <div class='row'>
-            <div class='col-8 p-5'>
-                <h1>Concept of component layout</h1>
-                <img class='img-fluid' src={layout} alt='Layout'/>
-            </div>
-            <div class='col-4 p-5'>
-            <img class='img-thumbnail' src={layout2} alt='ideas'/>
-            <img class='img-thumbnail' src={layout3} alt='posts'/>
+        <div className='container my-5'>
+            <div className='row'>
+                <div className='col-8 p-5'>
+                    <h1>Concept of component layout</h1>
+                    <img className='img-fluid' src={layout} alt='Layout' />
+                </div>
+                <div className='col-4 p-5'>
+                    <img className='img-thumbnail' src={layout2} alt='ideas' />
+                    <img className='img-thumbnail' src={layout3} alt='posts' />
+                </div>
             </div>
         </div>
-    </div>
     )
-    
+
 }
