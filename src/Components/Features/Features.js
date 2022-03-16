d665aa3f5383180950ce7c61fd786d1bd0255d4c
Andrew Kim <andrewk745@gmail.com>
2022-03-15 22:24:24 -0400

    css change

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -9,11 +9,11 @@ export default function Features() {
     return (
 <div class='container my-5'>
         <div class='row'>
-            <div class='col-8 p-4'>
+            <div class='col-8 p-5'>
                 <h1>Concept of component layout</h1>
                 <img class='img-fluid' src={layout} alt='Layout'/>
             </div>
-            <div class='col-4 p-4'>
+            <div class='col-4 p-5'>
             <h3>Layout will display map and graph</h3>
             <img class='img-thumbnail' src={layout2} alt='Layout2'/>
             <img class='img-thumbnail' src={layout3} alt='Layout3'/>
