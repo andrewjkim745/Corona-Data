3fb4bbc27d915ffb48070c911a9c6a79958b4625
Andrew Kim <andrewk745@gmail.com>
2022-03-16 22:50:23 -0400

    diseases api

--- src/Components/GraphData/graphData.js
+++ src/Components/GraphData/graphData.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react'
-
+import axios from 'axios'
 
 
 
 export default function graphData() {
 
     useEffect(() => {
-        
+        axios.get('https://disease.sh/v3/covid-19/all', {
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          });  
     }, [])
     return (
         <>
